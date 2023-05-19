ff601795ae835ded913d174794baa569d19d9682
luis09157 <luisk2ify@gmail.com>
2023-05-18 23:55:57 -0600

    insert usuario

---
+++ src/db/katsenbd.sql
@@ -0,0 +1,23 @@
+-- CREATE DATABASE IF NOT EXISTS katsendb;
+
+-- USE companydb;
+
+CREATE TABLE usuario (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  nombreUsuario VARCHAR(100),
+  contrasena VARCHAR(100),
+  nombre VARCHAR(45) ,
+  apellidoP VARCHAR(45) ,
+  apellidoM VARCHAR(45) ,
+  telefono INT(10),
+  direccion VARCHAR(45) ,
+  edad INT ,
+  email VARCHAR(45) ,
+  fechaAlta VARCHAR(45) ,
+  status int(1) ,
+  PRIMARY KEY(id)
+);
+
+
+INSERT INTO usuario(nombreUsuario, contrasena, nombre,apellidoP,apellidoM, telefono, direccion, edad, email, fechaAlta, status) 
+VALUES ('Siul09157', '1', 'Luis Alfonso', 'Nino', 'Martinez', 8115886425, 'San Jose #349 Col Santa Fe Apodaca NL', 28, 'luisk2ify@gmail.com', 1);
