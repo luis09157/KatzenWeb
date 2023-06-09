c85a567b741726024db00511616b3e99ad5a129b
DESKTOP-RISF858\sthef <luisk2ify@gmail.com>
2023-06-09 15:39:07 -0600

    localstorage

--- src/public/js/Pet/newPet.js
+++ src/public/js/Pet/newPet.js
@@ -20,6 +20,7 @@ $( document ).ready(function() {
 
 });
 
+
 function convertFormToJSON(form) {
     return $(form)
       .serializeArray()
--- src/public/js/User/login.js
+++ src/public/js/User/login.js
@@ -1,13 +1,30 @@
 $( document ).ready(function() {
-    console.log("animo amigos")
+    $("#btn_mensaje").hide();
+    isLoginSaved();
+
     $('#formLogin').on('submit', function(e) {
         e.preventDefault();
         var dataForm = convertFormToJSON($(this));        
         auth(dataForm);
     });
 
+    $('#recuerdameLog').change(function() {
+        if(!this.checked) {
+            localStorage.clear();
+        }
+              
+    });
+
 
 });
+
+function isLoginSaved(){
+    if(localStorage.getItem('username') != null && localStorage.getItem('password') != null){
+        $("#username").val(localStorage.getItem('username'));
+        $("#password").val(localStorage.getItem('password'));
+        $("#recuerdameLog").prop('checked', true)
+    }
+}
 
 function convertFormToJSON(form) {
     return $(form)
@@ -27,7 +44,21 @@ function auth(dataForm){
         dataType : 'json',
         data : dataForm ,
         success : function(result) {
-            console.log(result);
+           
+            console.log(result.data[0].EXITO)
+            if(result.data[0].EXITO == 1){
+                if($("#recuerdameLog").is(":checked")){
+                    localStorage.setItem('username', $("#username").val());
+                    localStorage.setItem('password', $("#password").val());
+                    localStorage.setItem('isLogin', true);
+                }
+                window.location.replace("/api/home");
+               
+                
+            }else{
+                $("#txt_mensaje").html(result.data[0].MENSAJE);
+                $("#btn_mensaje").click();
+            } 
         },
         error: function(xhr, resp, text) {
             console.log(xhr, resp, text);
